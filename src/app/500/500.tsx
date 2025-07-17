cdd9b1de7d26d77739fa45abc6c3b813d4bde140
davidv408 <davidv408@github.com>
2025-07-17 15:59:32 -0700

    Fix build issues

--- src/app/500/500.tsx
+++ src/app/500/500.tsx
@@ -1,4 +1,4 @@
-export const GlobalError = () => {
+export const Http500Page = () => {
   return (
     <section className="bg-white dark:bg-gray-900">
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
@@ -15,4 +15,4 @@ export const GlobalError = () => {
   );
 };
 
-export default GlobalError;
+export default Http500Page;
